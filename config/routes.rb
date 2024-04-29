544567f083478462e36661c5819292c5ba4486c3
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-04-29 17:00:04 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "pages#home"
-
+  resources :flats
+  resources :bookings, only: [:index, :show, :new, :create]
   # Defines the root path route ("/")
   # root "posts#index"
 end
