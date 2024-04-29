d18a362758bdc1832e98fa72d926af83f87fad8e
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-04-29 17:20:24 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating 3 flats..."
+
+puts "Finished!"
