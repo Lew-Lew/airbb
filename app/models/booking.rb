1cccf34102cc5d40e8c62e996c15b53d7997aad6
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-01 13:42:38 +0200

    validations booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  validates :start_date, :end_date, presence: true
+  validates :status, presence: true, inclusion: { in: ["pending", "confirmed", "declined"] }
 end
