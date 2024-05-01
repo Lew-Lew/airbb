f50773be42f60bb0d405ff81942e38e95c5bad28
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-01 15:59:00 +0200

    add geocoder gem

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,6 +1,8 @@
 class Flat < ApplicationRecord
+  geocoded_by :address
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
   validates :name, :description, :address, :price_per_night, presence: true
+  after_validation :geocode, if: :will_save_change_to_address?
 end
