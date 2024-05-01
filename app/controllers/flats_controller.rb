b92d2a19b06306e33661eaae188d60c923716c7c
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-01 16:39:53 +0200

    add searchbar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,9 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+    if params[:query].present?
+      @flats = @flats.where("name LIKE ?", "%#{params[:query]}%")
+    end
 
     # The `geocoded` scope filters only flats with coordinates
     @markers = @flats.geocoded.map do |flat|
