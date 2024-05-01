5a3be318a9a8d2599d2f6d315ed8eaa6d569a50f
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-01 16:21:59 +0200

    add mapbox and map to index and show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,22 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
     @flat = Flat.find(params[:id])
     @booking = Booking.new
+    @markers = [{
+      lat: @flat.latitude,
+      lng: @flat.longitude
+    }]
   end
 end
