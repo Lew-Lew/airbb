cf64c82d18b4e68c6983292037ab69c31be8b2ae
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-01 14:55:33 +0200

    validates bookings start date before end date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validates :status, presence: true, inclusion: { in: ["pending", "confirmed", "declined"] }
+  validate :start_date_before_end_date
+
+  private
+
+  def start_date_before_end_date
+    if start_date >= end_date
+      errors.add(:start_date, "must be before end date")
+    end
+  end
 end
